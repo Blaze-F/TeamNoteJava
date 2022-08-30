6228a40088d875e7e8cebef99c226d2231713497
JunH <101803254+Blaze-F@users.noreply.github.com>
2022-08-31 01:21:36 +0900

    FristNoteInit

---
+++ src/main/temp/SubarrayGivenSumBruteforce.java
@@ -0,0 +1,31 @@
+package src.main.temp;
+
+import java.util.Arrays;
+
+public class SubarrayGivenSumBruteforce {
+    public static int[] findSubarray(int[] a, int k) {
+        for(int i = 0; i < a.length; i++) {
+
+            int currentSum = a[i];
+
+            for(int j = i + 1; j <= a.length; j++) {
+
+                if (currentSum == k) {
+                    return Arrays.copyOfRange(a, i, j);
+                } else if (currentSum > k || j == a.length) {
+                    break;
+                }
+
+                currentSum += a[j];
+            }
+        }
+
+        return null;
+    }
+
+    public static void main (String[] args) {
+        int[] a = {4, 0, 11, 6, 1, 7};
+        int k = 8;
+        System.out.println(Arrays.toString(findSubarray(a, k)));
+    }
+}
